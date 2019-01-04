b1cdc37546472b468de64f60f94e68882a5d5f36
Josh Long <starbuxman@gmail.com>
2019-01-03 16:24:39 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ curl https://raw.githubusercontent.com/reactive-spring-book/publication/master/r
  	# if you want this to be freshly initialized then check in your work and delete the directory 
  else  	
  	echo "initializing ${dir_to_create}"  
- 	git_repo=https://github.com/reactive-spring-book/${d}.git
+	git_repo=git@github.com:reactive-spring-book/${d}.git
  	git clone ${git_repo} ${dir_to_create}
  fi
 done 
